d1f8b3ddd3143b83d927bef67506c969b313f4ed
porn <gi4884555@gmail.com>
2015-10-19 13:02:15 +0700

    เริ่มใช้ GitHub

---
+++ app/src/main/java/baac/nuntawit/pornpimon/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,18 @@
+package baac.nuntawit.pornpimon.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 19/10/15.
+ */
+public class MyOpenHelper {
+    // Explicit ประกาศค่าตัวแปร มี 3 แบบ
+    private static final String DATABASE_NAME="BAAC.db"; //สร้างค่าตัวแปร DATABASE_NAME เป็นแบบ private
+    private static final Integer DATABASE_VERSION = 1; //ต้องประกาศค่า version ของ database เสมอ
+
+
+    public MyOpenHelper(Context context) {
+
+
+    } //constructor
+} //Main Class
