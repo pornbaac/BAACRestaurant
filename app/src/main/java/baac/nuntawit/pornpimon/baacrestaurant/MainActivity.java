49035c2c73f664c3e482f770feca979480450e44
porn <gi4884555@gmail.com>
2015-10-20 15:47:31 +0700

    Finish Day2

--- app/src/main/java/baac/nuntawit/pornpimon/baacrestaurant/MainActivity.java
+++ app/src/main/java/baac/nuntawit/pornpimon/baacrestaurant/MainActivity.java
@@ -1,20 +1,47 @@
 package baac.nuntawit.pornpimon.baacrestaurant;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private UserTABLE objUserTABLE;
     private FoodTABLE objFoodTABLE;
-
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget ถูกตัวแปรที่ userEditText กับ wid
+        bindWidget();
+
+
+
 
     //Create & Connected Database สร้าง Method ย่อย
         createAndConnected(); //พิมพ์มี ()เป็น Method กด Alt+Enter ให้ระบบสร้าง Method ให้อัตโนมัติ
@@ -24,12 +51,155 @@ public class MainActivity extends AppCompatActivity {
 
      //Delete All SQLite
         DeleteAllSQLite();
+        //Syn
+        synJSONtoSQLite();
+
 
 
 
 
 
     } //Main Method
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void clickLogin(View view) {
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("")  || passwordString.equals("")) {
+
+            //Have Space
+            errorDialog("Have Space","Please Fill All Every blank");
+
+        } else {
+            //No Space
+            checkUser();
+        }
+    }
+
+    private void checkUser() {
+        try {
+            String[] strMyResult = objUserTABLE.searchUser(userString);
+            if (passwordString.equals(strMyResult[2])) {
+                Toast.makeText(MainActivity.this,"Welcome "+strMyResult[3],Toast.LENGTH_LONG).show();
+            } else {
+                errorDialog("Password False","Please Try again Password False");
+            }
+        } catch (Exception e) {
+            errorDialog("No This User","No "+ userString + " on my Database");
+        }
+
+
+    }
+
+    private void errorDialog(String strTitle, String strMessage) {
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(this);
+        objBuilder.setIcon(R.drawable.danger);
+        objBuilder.setTitle(strTitle);
+        objBuilder.setMessage(strMessage);
+        objBuilder.setCancelable(false);
+        objBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.show();
+    }
+
+
+    private void synJSONtoSQLite() {
+
+        //0. Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); //permitAll เปิด policy ทุกตัว
+        StrictMode.setThreadPolicy(myPolicy);
+
+
+        int intTimes = 0; //เริ่ม Syn ตามจำนวนตาราง
+        while (intTimes <=1) {
+            InputStream objInputStream = null;
+            String strJSON = null;
+            String strUserURL = "http://swiftcodingthai.com/baac/php_get_data_master.php"; //ตำแหน่ง url ที่มี JSON
+            String strFoodURL = "http://swiftcodingthai.com/baac/php_get_food.php";
+            HttpPost objHttpPost; //protocal ที่ใช้สำหรับ Apache Protocal คำสั่งมี ขีดฆ่าคำสั่ง
+
+            //1. Create InputStream
+            try {
+                // try ถ้า error ไม่ต้องหยุด code ให้ทำต่อไป
+                HttpClient objHttpClient = new DefaultHttpClient();
+                switch (intTimes) {
+                    case 0:
+
+                        objHttpPost = new HttpPost(strUserURL);
+                        break;
+                    case 1:
+
+                        objHttpPost = new HttpPost(strFoodURL);
+                        break;
+                    default: //กรณี Switch ไม่เท่ากับ 0 หรือ 1
+                        objHttpPost = new HttpPost(strUserURL);
+                        break;
+                }
+                //Create InputStream ต้องมี 3 บรรทัดด้านล่าง
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("baac", "InputStream ==>" + e.toString());
+            }
+
+            //2. Create JSON String
+            try {
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream,"UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null) {
+                    objStringBuilder.append(strLine);
+
+                } //while
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+            } catch (Exception e) {
+                Log.d("baac", "styJSON ==>" + e.toString());
+            }
+
+            //3. Update SQLite
+            try {
+                JSONArray objJsonArray = new JSONArray(strJSON);
+                for (int i = 0; i < objJsonArray.length(); i++) {
+                    JSONObject object = objJsonArray.getJSONObject(i);
+                    switch (intTimes) {
+                        case 0:
+                            // For userTABLE
+                            String strUser = object.getString("User");
+                            String strPassword = object.getString("Password");
+                            String strName = object.getString("Name");
+                            objUserTABLE.addNewUser(strUser, strPassword, strName);
+                            break;
+                        case 1:
+                            // For foodTABLE
+                            String strFood = object.getString("Food");
+                            String strSource = object.getString("Source");
+                            String strPrice = object.getString("Price");
+                            objFoodTABLE.addNewFood(strFood, strSource, strPrice);
+                            break;
+                    } //switch
+                } //for
+
+            } catch (Exception e) {
+                Log.d("baac", "Update ==>" + e.toString());
+            }
+            intTimes += 1;
+        }
+
+    } //Syn
 
     private void DeleteAllSQLite() {
 
--- app/src/main/java/baac/nuntawit/pornpimon/baacrestaurant/UserTABLE.java
+++ app/src/main/java/baac/nuntawit/pornpimon/baacrestaurant/UserTABLE.java
@@ -2,6 +2,7 @@ package baac.nuntawit.pornpimon.baacrestaurant;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
@@ -11,7 +12,7 @@ public class UserTABLE {
 
     //Explicit
     private MyOpenHelper objMyOpenHelper;
-    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+    private SQLiteDatabase writeSqLiteDatabase,readSqLiteDatabase;
 
     public static final String USER_TABLE = "userTABLE";
     public static final String COLUMN_ID_USER = "_id";
@@ -26,20 +27,48 @@ public class UserTABLE {
         writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
-
     } //Constructor
+
+    public String[] searchUser(String strUser) {
+        try {
+            String[] strResult = null;
+            Cursor objCursor = readSqLiteDatabase.query(USER_TABLE, new String[]{COLUMN_ID_USER, COLUMN_USER, COLUMN_PASSWORD, COLUMN_NAME},
+                    COLUMN_USER + "=?",
+                    new String[]{String.valueOf(strUser)},
+                    null,null,null,null);
+
+            if (objCursor != null) {
+                if (objCursor.moveToFirst()) {
+                    strResult = new String[objCursor.getColumnCount()];
+                    strResult[0] = objCursor.getString(0);
+                    strResult[1] = objCursor.getString(1);
+                    strResult[2] = objCursor.getString(2);
+                    strResult[3] = objCursor.getString(3);
+
+
+                } //if2
+            }   // if1
+            objCursor.close();
+            return strResult;
+
+            } catch (Exception e) {
+                return null;
+        }
+
+        //return new String[0];
+    }
+
 
 
     public long addNewUser(String strUser, String strPassword, String strName) {
 
         ContentValues objContentValues = new ContentValues(); //
-        objContentValues.put(COLUMN_USER,strUser); //ฉันต้องการเอา strUser ใส่ใน Column User
-        objContentValues.put(COLUMN_PASSWORD,strPassword);
-        objContentValues.put(COLUMN_NAME,strName);
+        objContentValues.put(COLUMN_USER, strUser); //ฉันต้องการเอา strUser ใส่ใน Column User
+        objContentValues.put(COLUMN_PASSWORD, strPassword);
+        objContentValues.put(COLUMN_NAME, strName);
 
-        return writeSqLiteDatabase.insert(USER_TABLE,null,objContentValues);
+        return writeSqLiteDatabase.insert(USER_TABLE, null, objContentValues);
     }
-
 
 
 } //Main Class
